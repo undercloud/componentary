e284840c5bf7e6148b882fa4a7139d7df8e80cdc
undercloud <lodashes@gmail.com>
2017-04-27 12:53:16 +0400

    imp

--- src/Invoke.php
+++ src/Invoke.php
@@ -1,16 +1,101 @@
 <?php
 namespace Componentary;
 
+/**
+ * External Invoke Builder
+ *
+ * @package  Componentary
+ * @author   undercloud <lodashes@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     http://github.com/undercloud/componentary
+ */
 class Invoke
 {
-    public function __construct()
+    private $name;
+
+    private $args = [];
+
+    public function __construct($name = 'undefinedInvoke')
     {
-    
-    
+        $this->name = $name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function hasArg($name)
+    {
+        return isset($this->args[$name]);
+    }
+
+    public function setArg($name, $val)
+    {
+        $this->args[$name] = $val;
+
+        return $this;
+    }
+
+    public function getArg($name)
+    {
+        if ($this->hasArg($name)) {
+        return $this->args[$name];
+        }
+    }
+
+    public function setArgs(array $args, $append = true)
+    {
+        $this->args = array_merge($append ? $this->args : [], $args);
+        
+        return $this;
+    }
+
+    public function getArgs()
+    {
+        return $this->args;
+    }
+
+    private function buildArgs()
+    {
+        return implode(',', array_map(function ($item) {
+            switch (gettype($item)) {
+            case 'NULL':
+                return 'null';
+
+            case 'boolean':
+                return $item ? 'true' : 'false';
+            break;
+
+            case 'string':
+                return "'{$item}'";
+            break;
+
+            case 'array':
+            case 'object':
+                return Helper::toJson($item);
+            break;
+
+            case 'integer':
+            case 'double':
+                return $item;
+            break;
+
+            default:
+                return 'undefined'; 
+            }
+        }, $this->args));
     }
 
     public function __toString()
     {
-    
+        return $this->name . '(' . $this->buildArgs() . ')';
     }
 }
