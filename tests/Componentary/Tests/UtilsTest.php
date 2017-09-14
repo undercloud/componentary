1646b7894f43f0cba883ff32dc9c22e405c6c9e4
mooden <alexandrostruses@yandex.ru>
2017-09-14 08:15:56 +0400

    utils

---
+++ tests/Componentary/Tests/UtilsTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Componentary\Tests;
+
+use Componentary\Utils;
+use PHPUnit_Framework_TestCase;
+
+class UtilsTest extends PHPUnit_Framework_TestCase
+{
+    public function testSimpleString()
+    {
+        $this->assertEquals(Utils::esc('<>"\'&'), '&lt;&gt;&quot;&#039;&amp;');
+        $this->assertEquals(Utils::unesc('&lt;&gt;&quot;&#039;&amp;'), '<>"\'&');
+
+        $this->assertEquals(Utils::stringify(true), 'true');
+        $this->assertEquals(Utils::stringify(0), '0');
+        $this->assertEquals(Utils::stringify(null), '');
+        $this->assertEquals(Utils::stringify(''), '');
+        $this->assertEquals(Utils::stringify([]), '[]');
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../src/AbstractDom.php';
 require_once __DIR__ . '/../src/AttributesTrait.php';
 require_once __DIR__ . '/../src/Element.php';
 require_once __DIR__ . '/../src/Component.php';
-require_once __DIR__ . '/../src/Helper.php';
+require_once __DIR__ . '/../src/Utils.php';
 require_once __DIR__ . '/../src/ClassList.php';
 require_once __DIR__ . '/../src/Style.php';
 require_once __DIR__ . '/../src/DomWalker.php';
