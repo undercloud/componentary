c0171bd0e6756a2ffd1950f27c837aad0e0c22f8
mooden <alexandrostruses@yandex.ru>
2019-02-14 08:20:42 +0400

    RenderException

---
+++ src/RenderException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Componentary;
+
+use Exception;
+
+/**
+ * RenderException
+ *
+ * @category Componentary
+ * @package  Beacon
+ * @author   undercloud <lodashes@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     http://github.com/undercloud/componentary
+ */
+class RenderException extends Exception {}
